8757fa2ab8e7ee9cc091d9d1b1c05bf346e66d08
Regan Dvoskin <rdvoskin@Regans-MacBook-Pro-2.local>
2015-11-11 13:27:53 -0500

    basic graphics stuff

---
+++ hw06/src/cs3500/music/view/MusicGuiViewPanel.java
@@ -0,0 +1,24 @@
+package cs3500.music.view;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+
+import javax.swing.*;
+
+
+/**
+ * Created by rdvoskin on 11/11/15.
+ */
+public class MusicGuiViewPanel extends JPanel {
+
+  @Override
+  public void paint(Graphics g){
+    // Look for more documentation about the Graphics class,
+    // and methods on it that may be useful
+    g.drawString("Hello World", 25, 25);
+  }
+
+}
