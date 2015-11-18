2d1bc13df27f946eed1ecee0cb42802b5a1e93f7
isaacfernandez <isaac.fernandez2@gmail.com>
2015-11-18 17:28:17 -0500

    Started on function objects

---
+++ hw06/src/cs3500/music/controller/KeyboardHandlerFunctionObjects.java
@@ -0,0 +1,55 @@
+package cs3500.music.controller;
+
+/**
+ * Created by isaacf on 11/18/15.
+ */
+public class KeyboardHandlerFunctionObjects {
+}
+
+class Scroll implements Runnable {
+
+  /**
+   * When an object implementing interface <code>Runnable</code> is used to create a thread,
+   * starting the thread causes the object's <code>run</code> method to be called in that separately
+   * executing thread. <p> The general contract of the method <code>run</code> is that it may take
+   * any action whatsoever.
+   *
+   * @see Thread#run()
+   */
+  @Override
+  public void run() {
+
+  }
+}
+
+class JumpToBeginning implements Runnable {
+
+  /**
+   * When an object implementing interface <code>Runnable</code> is used to create a thread,
+   * starting the thread causes the object's <code>run</code> method to be called in that separately
+   * executing thread. <p> The general contract of the method <code>run</code> is that it may take
+   * any action whatsoever.
+   *
+   * @see Thread#run()
+   */
+  @Override
+  public void run() {
+
+  }
+}
+
+class JumpToEnd implements Runnable {
+
+  /**
+   * When an object implementing interface <code>Runnable</code> is used to create a thread,
+   * starting the thread causes the object's <code>run</code> method to be called in that separately
+   * executing thread. <p> The general contract of the method <code>run</code> is that it may take
+   * any action whatsoever.
+   *
+   * @see Thread#run()
+   */
+  @Override
+  public void run() {
+
+  }
+}
