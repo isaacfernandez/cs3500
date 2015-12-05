e059a8cfbb3a4025856f248559358dc253828c91
Regan Dvoskin <rdvoskin@Regans-MacBook-Pro-2.local>
2015-12-05 15:05:29 -0500

    fuuuuck

--- hw06/src/cs3500/music/model/ToneToNoteAdapter.java
+++ hw06/src/cs3500/music/model/ToneToNoteAdapter.java
@@ -5,4 +5,63 @@ package cs3500.music.model;
  */
 public class ToneToNoteAdapter extends ToneImp implements Note{
 
+  //TODO?
+  @Override
+  public void setVolume(int vol) {
+    //they never use setVolume in the view, volume is final
+  }
+
+  //TODO?
+  @Override
+  public void setInstrument(int instrument) {
+    //they never use setInstrument in the view, instrument is final
+  }
+
+  //TODO
+  @Override
+  public String getPitchInOctave() {
+    //what does this do?
+  }
+
+  //TODO
+  @Override
+  public int getStartBeat() {
+    return 0;
+  }
+
+  //TODO
+  @Override
+  public void setStartBeat(int beat) throws IllegalArgumentException {
+    //this is gonna suck
+  }
+
+  //TODO
+  @Override
+  public int getEndBeat() {
+    return 0;
+  }
+
+  //TODO
+  @Override
+  public void setEndBeat(int beat) throws IllegalArgumentException {
+    //also gonna suck
+  }
+
+  //TODO
+  @Override
+  public int getPitch() {
+    return 0; //fffffffuuu
+  }
+
+  //TODO
+  @Override
+  public void setPitch(int pitch) throws IllegalArgumentException {
+    //what
+  }
+
+  //??? TODO
+  @Override
+  public boolean starting(int beat) {
+    return false;
+  }
 }
